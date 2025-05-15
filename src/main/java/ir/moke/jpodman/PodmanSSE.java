05df40f88cb55c1604d196a8d53fad605672f0fc
Mahdi <mah454@mail.moke.ir>
2025-05-15 13:57:16 +0330

    SSE ExecutorService setter

--- src/main/java/ir/moke/jpodman/PodmanSSE.java
+++ src/main/java/ir/moke/jpodman/PodmanSSE.java
@@ -110,6 +110,10 @@ public class PodmanSSE implements Closeable {
         }
     }
 
+    public ExecutorService getExecutorService() {
+        return executorService;
+    }
+
     @Override
     public void close() {
         if (executorService != null) {
